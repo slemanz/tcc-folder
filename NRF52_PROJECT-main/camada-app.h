d20cfa19c72d8d50ab72417e265dd8a3b14732b6
slemanz <william_sleman@hotmail.com>
2025-05-23 05:22:56 -0300

    feat: apendice d

--- NRF52_PROJECT-main/camada-app.h
+++ NRF52_PROJECT-main/camada-app.h
@@ -4,22 +4,22 @@
 
 #include <stdint.h>
 
-#define CPU_FREQ        (64000000)
-#define SYSTICK_FREQ    (1000) // in hz (1000hz == every 1 ms)
+#define CPU_FREQ            (64000000)
+#define SYSTICK_FREQ        (1000) // in Hz
 
 #define LED_BLUE_PIN        GPIO_PIN_NO_6
 #define LED_GREEN_PIN       GPIO_PIN_NO_16
 #define LED_RED_PIN         GPIO_PIN_NO_24
 #define LED_BUILT_IN        GPIO_PIN_NO_13
 #define LED1_EXT_PIN        GPIO_PIN_NO_5
-#define BUTTON1_PORT           GPIOP1
-#define BUTTON1_PIN            GPIO_PIN_NO_2
+#define BUTTON1_PORT        GPIOP1
+#define BUTTON1_PIN         GPIO_PIN_NO_2
 
 // UART
-#define UART_TX_PORT           GPIOP1
-#define UART_TX_PIN            GPIO_PIN_NO_10
-#define UART_RX_PORT           GPIOP1
-#define UART_RX_PIN            GPIO_PIN_NO_3
+#define UART_TX_PORT        GPIOP1
+#define UART_TX_PIN         GPIO_PIN_NO_10
+#define UART_RX_PORT        GPIOP1
+#define UART_RX_PIN         GPIO_PIN_NO_3
 
 // SPI
 #define CS_PORT             GPIOP1
@@ -32,10 +32,10 @@
 #define SPI_PIN_MOSI        GPIO_PIN_NO_27
 
 // TWI 
-#define TWI_SDA_PORT           GPIOP0
-#define TWI_SDA_PIN            GPIO_PIN_NO_14
-#define TWI_SCL_PORT           GPIOP0
-#define TWI_SCL_PIN            GPIO_PIN_NO_15
+#define TWI_SDA_PORT        GPIOP0
+#define TWI_SDA_PIN         GPIO_PIN_NO_14
+#define TWI_SCL_PORT        GPIOP0
+#define TWI_SCL_PIN         GPIO_PIN_NO_15
 
 
 void system_waitStable(void);
